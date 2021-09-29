cf47f275a73187b9a2a23b394928fcca6c510f37
arbins1919 <gu_xinlan@163.com>
2021-09-30 01:29:47 +0800

    Commit task1.

--- example/Scene.java
+++ example/Scene.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Scene {
 
     public static void main(String[] args) throws IOException {
-
+        //创建一条有七个点的line，为7个gourd随机分配位置
         Line line = new Line(7);
         line.put(Gourd.ONE, 6);
         line.put(Gourd.TWO, 3);
@@ -17,10 +17,11 @@ public class Scene {
         line.put(Gourd.SIX, 4);
         line.put(Gourd.SEVEN, 0);
 
+        //获取爷爷单例
         Geezer theGeezer = Geezer.getTheGeezer();
-
+        //创建排序器
         Sorter sorter = new BubbleSorter();
-
+        //为爷爷配置排序器
         theGeezer.setSorter(sorter);
 
         String log = theGeezer.lineUp(line);
